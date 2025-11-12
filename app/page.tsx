5785a03f224cc3deee9198b1f7b18bc9128d5393
홍범선 <qjatjs123123@naver.com>
2025-11-12 15:20:51 +0900

    feat: Button 공통 컴포넌트 구현

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import { Text } from "@/src/shared/ui/Text";
+import { Button } from "@/src/shared/ui/Button/Button";
+import { Text } from "@/src/shared/ui/Text/Text";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Text weight="bold" size="1" type="title">123</Text>
+      <Button type="primary" style="fill">관심기업 생성</Button>
+      <Button type="primary" style="outline">관심기업 생성</Button>
     </>
   );
 }
---
+++ src/shared/ui/Button/Button.tsx
@@ -0,0 +1,46 @@
+import classNames from "classnames";
+import { PropsWithChildren } from "react";
+import styles from "./Button.module.css";
+
+type Props = PropsWithChildren<{
+  type?: "primary" | "default";
+  style?: "fill" | "outline";
+  size?: "big" | "medium";
+  className?: string;
+  onClick?: React.MouseEventHandler<HTMLElement>;
+  disabled?: boolean;
+  dataCypress?: string;
+}>;
+
+export function Button(props: Props) {
+  const {
+    type = "primary",
+    style = "outline",
+    size = "medium",
+    disabled,
+    children,
+    className,
+    dataCypress,
+    ...rest
+  } = props;
+
+  return (
+    <button
+      type="button"
+      data-cy={dataCypress}
+      className={classNames(
+        styles.button,
+        styles[`button--type-${type}`],
+        styles[`button--style-${style}`],
+        styles[`button--size-${size}`],
+        styles[`button--rd`],
+        "font-semibold",
+        className
+      )}
+      disabled={disabled}
+      {...rest}
+    >
+      <span className="button__content">{children}</span>
+    </button>
+  );
+}
