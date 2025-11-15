60558392aa0ea15ac5df3783a376b2d9e7d5280e
홍범선 <qjatjs123123@naver.com>
2025-11-15 21:32:29 +0900

    fix: date 포맷 수정

--- src/widgets/Manager/ui/common/TableCell.tsx
+++ src/widgets/Manager/ui/common/TableCell.tsx
@@ -6,6 +6,7 @@ import { FavoriteCompany } from "@/entities/Manager";
 import { Button } from "@/shared/ui/Button/Button";
 import { useDeleteFavoriteCompanyOne } from "@/features/Manager/deleteFavoriteCompany";
 import { DetailModalEntry } from "@/features/Manager/readFavoriteCompany";
+import { formatDate } from "@/shared/lib/formatDate";
 
 interface FavoriteCompanyCellProps {
   item: FavoriteCompany;
@@ -28,7 +29,7 @@ export const TableCell = React.memo(function TableCell({
             <CheckBox checked={checked} onChange={() => onChange(item.id)} />
           </Table.Cell>
           <Table.Cell>{item.company_name}</Table.Cell>
-          <Table.Cell>{item.created_at}</Table.Cell>
+          <Table.Cell>{formatDate(item.created_at)}</Table.Cell>
           <Table.Cell>
             <Button
               className="!p-0 !border-0"
