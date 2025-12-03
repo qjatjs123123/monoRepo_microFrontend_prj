457cb9b8b2ed9db741aa9b86031887e6575ecdd0
홍범선 <qjatjs123123@naver.com>
2025-12-03 17:23:10 +0900

    feat: link 에 맞게 링크 스타일 변경

--- apps/header/src/Headers.tsx
+++ apps/header/src/Headers.tsx
@@ -1,22 +1,26 @@
 import { Text } from "@monorepo/ui";
 import Logo from "@monorepo/ui/public/logo.png";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import "./global.css";
 
 export default function Headers() {
+  const { pathname } = useLocation();
+
+  const getWeight = (path: string) => (pathname === path ? "bold" : "normal");
+
   return (
     <div className="header:flex header:justify-between header:w-full header:py-5">
       <img src={Logo} alt="회사 로고" width={117} height={24} />
 
       <div className="header:flex header:gap-5">
         <Link to="/finance">
-          <Text type="body" weight="bold">
+          <Text type="body" weight={getWeight("/finance")}>
             일반과제
           </Text>
         </Link>
 
         <Link to="/viewer">
-          <Text type="body" weight="normal">
+          <Text type="body" weight={getWeight("/viewer")}>
             산업 전문화 과제
           </Text>
         </Link>
--- apps/header/src/main.tsx
+++ apps/header/src/main.tsx
@@ -2,10 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./global.css";
-// import { Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>
 );
